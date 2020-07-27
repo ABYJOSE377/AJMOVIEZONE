ddd31859b3cabd0c52339e10afbfc169eaa0924b
ABYJOSE377 <64739511+ABYJOSE377@users.noreply.github.com>
2020-07-27 12:40:13 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,11 +18,9 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-ഞാൻ ഒരു ഗ്രൂപ്പ് മാനേജർ ബോട്ട് ആണ് പക്ഷെ വെറുതെ സമയം കളയണ്ട 
-എന്നെ AJ MOVIE ZONE ഗ്രൂപ്പിൽ 
-മാത്രമേ ഉപയോഗിക്കാൻ കഴിയൂ
-[Group](buttonurl:https://t.me/joinchat/NFOrmRv4Lh7RkJ40phJkkw)
-[Channel](buttonurl:https://t.me/aj_full_movies:same)
+ഞാൻ ഒരു ഗ്രൂപ്പ് മാനേജർ ബോട്ട് ആണ് പക്ഷെ വെറുതെ സമയം കളയണ്ട എന്നെ AJ MOVIE ZONE ഗ്രൂപ്പിൽ മാത്രമേ ഉപയോഗിക്കാൻ കഴിയൂ
+Group - https://t.me/joinchat/NFOrmRv4Lh7RkJ40phJkkw
+Channel - https://t.me/aj_full_movies
 """
 
 HELP_STRINGS = """
